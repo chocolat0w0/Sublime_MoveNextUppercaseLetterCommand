f5aaa927a14e18ff0815260c88da35456f819a9e
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2013-10-13 16:50:42 +0900

    add select mode

--- move_nexe_uppercase_letter_command.py
+++ move_nexe_uppercase_letter_command.py
@@ -1,18 +1,23 @@
 import sublime, sublime_plugin
 import re
 
+def isSelectedMode(view):
+	return view.sel()[0].begin() != view.sel()[0].end()
+
 class MoveNextUppercaseLetterCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for region in self.view.sel():
-			word = self.view.word(region)
 			line = self.view.line(region)
-			current_point = region.begin()
-			word_start_point = word.begin()
+			current_start_point = region.begin()
+			current_end_point = region.end()
 			line_start_point = line.begin()
-			m = re.search("[A-Z]", self.view.substr(line)[current_point - line_start_point + 1:])
+			m = re.search("[A-Z]", self.view.substr(line)[current_end_point - line_start_point + 1:])
 
+			move_to_point = current_end_point + m.start() + 1
 			self.view.sel().clear()
-			move_to_point = current_point + m.start() + 1
-			self.view.sel().add(move_to_point)
+			if (isSelectedMode(self.view)):
+				self.view.sel().add(sublime.Region(current_start_point, move_to_point))
+			else:
+				self.view.sel().add(move_to_point)
 
 
